87152f492c943551f490c9398c81df96c5439d18
genath3 <georgienathan3@gmail.com>
2025-06-28 15:57:42 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,15 +22,15 @@ from huggingface_hub import hf_hub_download
 def load_data():
     from huggingface_hub import hf_hub_download
     file_path = hf_hub_download(
-        repo_id="7ng10dpE",
-        filename="Online-Retail/xiaomi_cleaned.csv",
+        repo_id="7ng10dpE/Online-Retail",
+        filename="xiaomi_cleaned.csv",
         token=st.secrets["huggingface"]["token"]
     )
     df = pd.read_csv(file_path)
     df["event_time"] = pd.to_datetime(df["event_time"], errors="coerce")
     df["brand"] = df["brand"].astype(str).str.lower()
     df = df[df["brand"] == "xiaomi"]
-    return df      
+    return df   
       
 
 df = load_data()
